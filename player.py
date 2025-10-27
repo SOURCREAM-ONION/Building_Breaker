6786e2cb40993e7216374e672f17f9aff56f7738
Dongil Park <rmaksssss@gmail.com>
2025-10-27 19:20:23 +0900

    칼과 캐릭터 동시에 보이도록 구현

--- player.py
+++ player.py
@@ -4,23 +4,17 @@ open_canvas()
 
 character = load_image('char1_1.png')
 sword = load_image('basic_sword.png')
-'''
-frame = 0
-while True:
-    clear_canvas()
-    character.clip_draw(frame * 30, 30, 30, 30, 400, 300, 50, 50)
-    frame = (frame + 1) % 3
-    update_canvas()
-    delay(0.1)
-'''
 
-frame = 0
+framec = 0;
+frames = 0
 while True:
     clear_canvas()
-    sword.clip_draw(frame * 203, 0, 203, 350, 400, 300, 50, 50)
-    frame = (frame + 1) % 10
+    character.clip_draw(framec * 30, 30, 30, 30, 400, 300, 50, 50)
+    framec = (framec + 1) % 3
+    sword.clip_draw(frames * 203, 0, 203, 350, 410, 310, 50, 50)
+    frames = (frames + 1) % 10
     update_canvas()
-    delay(0.1)
+    delay(0.3)
 
 
 close_canvas()
