b39a0ba198989236bd973bd309a032d9f5ded81b
Dongil Park <rmaksssss@gmail.com>
2025-11-03 17:13:49 +0900

    주석 추가 및 함수와 클래스 위치 조정 및 오타 수정

--- player.py
+++ player.py
@@ -1,22 +1,29 @@
 from pico2d import *
 
-
-
-
-
 class Character:
-    def __init__(self):
-        self.image = load_image('char1_1.png')
+    def __init__(self): # 캐릭터가 처음 생성될 때 나오는 부분
+        self.x, self.y = 400, 300
         self.frame = 0
-        self.x, self.y = 400, 30
-    def draw(self):
+        self.image = load_image('Char1_1.png')
+
+    def draw(self):  # 캐릭터가 그려지는 부분
         self.frame = (self.frame + 1) % 3
-    def update(self):
-        self.image.clip_draw(self.frame * 30, 30, 30, 30, 400, 300, 50, 50)
+
+    def update(self): # 캐릭터가 업데이트 되는 부분
+        self.image.clip_draw(self.frame * 30, 30, 30, 30, 400, 300)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 open_canvas()
 
-def reset_world():
+def reset_world(): # 월드가 새로 나올때 그려지는 부분
     global running
     global character
     global world
@@ -28,26 +35,18 @@ def reset_world():
 
 reset_world()
 
-def update_world():
+def update_world(): # 월드에 객체가 추가되는 부분
     for game_object in world:
         game_object.update()
 
 
-def render_world():
+def render_world(): # 월드가 만들어지는 부분
     clear_canvas()
     for game_object in world:
         game_object.draw()
     update_canvas()
 
 
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
 
 
 
