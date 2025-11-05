3cc8dc9e628903a0498b15367d1ddb746257cf61
Dongil Park <rmaksssss@gmail.com>
2025-11-05 11:48:55 +0900

    state_machine객체 update함수 추가

--- state_machine.py
+++ state_machine.py
@@ -2,3 +2,6 @@ class StateMachine:
     def __init__(self, start_state):
         self.current_state = start_state
         self.current_state.enter()
+
+    def update(self):
+        self.current_state.do()
