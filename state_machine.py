b9dfad51b5bb08a208f9f664c8901fefe201e740
Dongil Park <rmaksssss@gmail.com>
2025-11-05 11:55:31 +0900

    StateMachine 객체 draw함수 추가 및 구현

--- state_machine.py
+++ state_machine.py
@@ -4,4 +4,7 @@ class StateMachine:
         self.current_state.enter()
 
     def update(self):
-        self.current_state.do()
+        self.current_state.do() # 현재 상태의 do 메서드 호출
+
+    def draw(self):
+        self.current_state.draw() # 현재 상태의 draw 메서드 호출
