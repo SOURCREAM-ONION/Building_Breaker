1c7289fbebddb9054a7b0f04cab625d123059bea
Dongil Park <rmaksssss@gmail.com>
2025-11-10 23:23:36 +0900

    이미지 나오는지 테스트

--- sword.py
+++ sword.py
@@ -6,13 +6,13 @@ class Sword:
     def __init__(self):
         self.x, self.y = 400, 90 # 검의 초기 위치
         self.image = load_image('basic_sword.png') # 검의 이미지 로드
-        self.state_machine = StateMachine()
+
 
     def update(self):
-        self.state_machine.update() # 상태 머신의 업데이트 호출
+        pass
 
     def draw(self):
-        self.image.draw(self.x, self.y) # 검의 이미지 그리기
+        self.image.draw(self.x, self.y)
 
     def handle_event(self, event):
-        self.state_machine.handle_event(event) # 상태 머신의 이벤트 처리 호출
+        pass
