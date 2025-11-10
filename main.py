6d299266a4f930568317d4d8f5156bc66a013861
Dongil Park <rmaksssss@gmail.com>
2025-11-10 22:33:34 +0900

    Sword 클래스 생성

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from pico2d import *
 
 from character import Character
 
+class Sword:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        pass
+
 def handle_events():
     global running
     events = get_events()
