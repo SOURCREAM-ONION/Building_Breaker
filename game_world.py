d7249ec98118850a6b7bf5ed56e9c8ea157513e9
Dongil Park <rmaksssss@gmail.com>
2025-11-11 11:50:53 +0900

    game_world에서 기존 수업에 배웠던 layer 코드 추가 (나중에 필요한 부분만 수정 사용예정)

---
+++ game_world.py
@@ -0,0 +1,24 @@
+# world[0] : 가장 낮은 계층 layer
+# world[1] : 그 다음 계층 layer
+# world[2] : 가장 높은 계층 layer
+world = [[], [], []]
+
+def add_object(o, depth=0):
+    world[depth].append(o)
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    print("삭제할 월드가 없습니다")
