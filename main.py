00ae1a91281c907fff37458767f7caebc3d5b3d9
Dongil Park <rmaksssss@gmail.com>
2025-11-11 23:33:00 +0900

    game_framework를 이용해서 logo_mode 구현

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from pico2d import *
 import logo_mode
+import game_framework
 
 open_canvas()
-logo_mode.init()
-while logo_mode.running:
-    logo_mode.handle_events()
-    logo_mode.update()
-    logo_mode.draw()
-    delay(0.07)
-logo_mode.finish()
+game_framework.run(logo_mode)
 close_canvas()
