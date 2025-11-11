931ab953e04569edac84170899ce8a991cb32ef6
Dongil Park <rmaksssss@gmail.com>
2025-11-11 23:05:28 +0900

    main.py 생성

---
+++ main.py
@@ -0,0 +1,12 @@
+from pico2d import *
+import play_mode
+
+open_canvas()
+play_mode.init()
+while play_mode.running:
+    play_mode.handle_events()
+    play_mode.update()
+    play_mode.draw()
+    delay(0.07)
+play_mode.finish()
+close_canvas()
