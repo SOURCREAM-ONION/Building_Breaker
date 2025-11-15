50df5a6a92eebcf71962496c3b8cb134020e8ba7
Dongil Park <rmaksssss@gmail.com>
2025-11-15 23:35:15 +0900

    sword의 y값 위치 조정

--- sword.py
+++ sword.py
@@ -24,7 +24,7 @@ class Idle_Sword:
 
 class Wield_Sword:
     def __init__(self, sword):
-        self.y = 96
+        self.y = 39
         self.sword = sword
         self.frame = 0
         self.frame_count = 6
@@ -57,7 +57,7 @@ class Wield_Sword:
 
 class Sword:
     def __init__(self):
-        self.x, self.y = 205, 87 # 검의 초기 위치
+        self.x, self.y = 205, 27 # 검의 초기 위치
         self.image = load_image('basic_sword.png') # 검의 이미지 로드
         self.IDLE_SWORD = Idle_Sword(self)
         self.WIELD_SWORD = Wield_Sword(self)
