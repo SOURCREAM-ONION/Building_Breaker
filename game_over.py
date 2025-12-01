5eb2c6b6c3c6aeec770921c7bdfac7dd2e9fcb71
Dongil Park <rmaksssss@gmail.com>
2025-12-01 22:56:16 +0900

    game_over 파일 추가 및 함수 생성

---
+++ game_over.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import game_framework
+import play_mode
+import title_mode
+
+image = None
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def handle_events():
+    pass
+
+def draw():
+    pass
