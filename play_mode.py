1557abda6cc122656b104a6ac7335c255aab80c7
Dongil Park <rmaksssss@gmail.com>
2025-12-03 21:53:54 +0900

    빌딩을 일정 시간 뒤에 스폰 시 빌딩이 겹치는 현상이 생겨 빌딩이 파괴되면 새 빌딩이 나오도록 구현

--- play_mode.py
+++ play_mode.py
@@ -74,12 +74,36 @@ def update():  # 월드에 객체가 추가되는 부분
                         game_framework.change_mode(game_over)
                         return
 
-    # 빌딩 스폰 타이머 업데이트
-    spawn_timer += game_framework.frame_time
-    if spawn_timer >= 15.0:
-        spawn_timer = 0.0
-        new_building = create_random_building()
-        game_world.add_object(new_building, 0)
+    # # 빌딩 스폰 타이머 업데이트
+    # spawn_timer += game_framework.frame_time
+    # if spawn_timer >= 15.0:
+    #     spawn_timer = 0.0
+    #     new_building = create_random_building()
+    #     game_world.add_object(new_building, 0)
+
+    # 완전히 파괴한 빌딩 제거
+    for obj  in list(game_world.world[0]):
+        if isinstance(obj, Building):
+            all_destroyed = True
+            for floor in obj.floors:
+                if floor['alive']: # 층이 하나라도 살아있다면
+                    all_destroyed = False # all_destroyed를 False로
+                    break
+
+            if all_destroyed: # 모든 층이 파괴되었다면
+                game_world.remove_object(obj) # 빌딩 제거
+
+    # 빌딩이 하나도 없으면 새 빌딩 생성
+    building_exists = False
+    for obj in game_world.world[0]:
+        if isinstance(obj, Building):
+            building_exists = True
+            break
+
+    if not building_exists: # 빌딩이 없으면
+        new_building = create_random_building() # 새 빌딩 생성
+        game_world.add_object(new_building, 0) # 월드에 추가
+        print("새 빌딩 생성됨") # 디버그 메시지
 
     # 검 공격 중일 때 모든 빌딩과 충돌 체크
     if sword.is_attacking():
