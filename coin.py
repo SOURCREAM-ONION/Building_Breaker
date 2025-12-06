6bc2fbfbbb8995d9c65b39f1197be40aa0ce79ac
Dongil Park <rmaksssss@gmail.com>
2025-12-06 15:31:10 +0900

    coin 파일 추가 및 클래스 구현 ( 이미지 작동만 가능 )

---
+++ coin.py
@@ -0,0 +1,23 @@
+from pico2d import *
+
+class Coin:
+    image = None
+
+    def __init__(self, x = 400, y = 300):
+        if Coin.image is None:
+            Coin.image = load_image('ui/coin.png')
+        self.x = x
+        self.y = y
+        self.frame = 0
+        self.total_frames = 4
+        self.animation_speed = 30
+        self.delay_counter = 0
+
+    def update(self):
+        self.delay_counter += 1
+        if self.delay_counter >= self.animation_speed:
+            self.frame = (self.frame + 1) % self.total_frames
+            self.delay_counter = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 1067, 0, 1067, 1067, self.x, self.y, 100, 100)
